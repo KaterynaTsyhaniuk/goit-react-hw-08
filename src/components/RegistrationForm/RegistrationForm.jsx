f3f7fea786c3e138053f100d7ac12b1d2f8ceb78
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-12-11 16:23:56 +0200

    add register thunk

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,9 +1,22 @@
 import { Field, Form, Formik } from "formik";
+import { useDispatch } from "react-redux";
+import { register } from "../../redux/auth/operations";
 
 const RegistrationForm = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = (values, options) => {
+    dispatch(register(values));
+    options.resetForm();
+  };
+
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
   return (
     <div>
-      <Formik>
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
         <Form>
           <Field name="name" placeholder="Enter name" />
           <Field
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,15 +2,19 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const goitApi = axios.create({
-  baseUrl: "https://connections-api.goit.global/",
+  baseURL: "https://connections-api.goit.global/",
 });
 
 export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
     try {
+      console.log("Sending request with credentials:", credentials);
       const response = await goitApi.post("/users/signup", credentials);
+      console.log("Response data:", response.data);
+      return response.data;
     } catch (error) {
+      console.error("Error message:", error.message);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
